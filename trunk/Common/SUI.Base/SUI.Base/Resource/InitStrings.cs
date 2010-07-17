11a9f0846c543fd62538064f794445974297fa5c
cswangpeng <cswangpeng@e1525d2e-e059-921f-43f9-af4582241750>
2010-07-17 07:10:52 +0000

    Modify SUILogger.cs support customized log file location

--- trunk/Common/SUI.Base/SUI.Base/Resource/InitStrings.cs
+++ trunk/Common/SUI.Base/SUI.Base/Resource/InitStrings.cs
@@ -8,12 +8,7 @@ namespace SUI.Base.Resource
 {
     public class InitStrings
     {
-        public static string SUI_HOME = Environment.GetEnvironmentVariable("SUI_HOME", EnvironmentVariableTarget.User);
-        public static string LogFile = Path.Combine(SUI_HOME, "Log\\SUIBaseLog.log");
-
-        // Hard code here for now!
-        public static string WinFormViewerResource = @"C:\Program Files\Common Files\Business Objects\2.7\Managed\xx\CrystalDecisions.Windows.Forms.Resource.dll";
-
+        public static string LogFile = "Running.log";
         public static string WOW6432 = SUIUtil.IsX64 ? @"\Wow6432Node" : "";
     }
 }
